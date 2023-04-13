c6d64d28ba8e120df81340fe122802a5924ea804
Lucas Cardoso <lucas.gavirachi@edu.pucrs.br>
2023-04-13 15:25:34 -0300

    Bombou com amigo vitor

--- vigenere.py
+++ vigenere.py
@@ -1,25 +1,219 @@
 from typing import List
 from math import isclose
 from collections import Counter
+import sys
+import string
 
-####################### Descobrir o tamanho da chave - Indice de coinciencia #######################
-with open('./texts/english.txt') as f:
-    encrypted_text = f.read()
-# Text
-encrypted_text = encrypted_text.replace(" ", "").upper()
+# Alphabet vector starting from A until Z
+ALPHABET = string.ascii_uppercase
 
-# Max number of key tests
-max_key_size = 20
 
-# Alphabet
-alph = list(map(chr, range(ord('A'), ord('Z')+1)))
+def main():
+    print("Starting parameters...")
+    # starting parameters
+    # Max number of key tests
+    max_key_size = 20
+    if len(sys.argv) <= 1:
+        print("Missing arguments. Exiting. Usage: vigenere.py path/to/file.text")
+        exit()
+    # Encrypted text from file
+    encrypted_text = read_encrypted_file(file_path=sys.argv[1])
+    # Text decoded
+    plain_text = ""
 
-# Coincidende Indexes
-# english_index = 0.0667
-# portuguese_index = 0.0745
+    print("Breaking Vigenere Cypher...")
+    # Starting
+    # Finding possible key lengths
+    possible_key_lengths, language = find_key_length_and_language(
+        max_key_size, encrypted_text)
+    print(f"Language: ", language)
+    # Finding possible keys
+    possible_keys = list(
+        set(find_key(possible_key_lengths, language, encrypted_text)))
 
-# 1 2 3 1 2 3, 2
-# [[1, 1] [2, 2] ]
+    # User prompt
+    invalid_prompt = True
+    while (invalid_prompt):
+        print("Please select the key you want to use to decrypt the text, using the number options: ")
+        for i in range(0, len(possible_keys)):
+            print(f'{i} - {possible_keys[i]}')
+        key_index = input("Please enter the number of the key: ")
+
+        # Validating user prompt
+        try:
+            key_index = int(key_index)
+            invalid_prompt = False
+        except Exception:
+            invalid_prompt = True
+
+        if invalid_prompt:
+            continue
+        if key_index >= len(possible_keys) or key_index < 0:
+            invalid_prompt = True
+        else:
+            invalid_prompt = False
+
+    plain_text = decrypt_vigenere(
+        possible_keys[int(key_index)], encrypted_text)
+    print("Writing file with plain text...")
+    # open text file
+    text_file = open("decoded_text.txt", "w")
+
+    # write string to file
+    text_file.write(plain_text)
+
+    # close file
+    text_file.close()
+    print("Done!")
+
+
+def decrypt_vigenere(key: str = "", encrypted_text: str = "") -> str:
+    print(f'Decoding using key: ', key)
+    ######## Descriptografar o texto usando a chave encontrada #########
+    key_length = len(key)
+    plain_text = ''
+
+    # Decrypt the ciphertext using the computed key
+    for i in range(len(encrypted_text)):
+        ciphertext_char = encrypted_text[i]
+        key_char = key[i % key_length]
+        plain_char = ALPHABET[(ALPHABET.index(
+            ciphertext_char) - ALPHABET.index(key_char)) % 26]
+        plain_text += plain_char
+
+    return plain_text
+
+
+def find_key(possible_key_lengths: list[int] = [7], language: str = 'english', cipher_text: str = "") -> list[str]:
+    print("Finding possible keys...")
+
+    ########## Descobrindo a chave ##########
+    most_frequent_letters = ['E', 'T', 'A']
+
+    if (language == 'portuguese'):
+        most_frequent_letters = ['A', 'E', 'O']
+
+    possible_keys: list = []
+
+    for key_length in possible_key_lengths:
+
+        # Quebrando o texto cifrado em segmentos de tamanho:
+        # seg[len(cipher_text) / key_length][key_length]
+        segments = [cipher_text[i::key_length] for i in range(key_length)]
+
+        # Olhando cada uma das colunas
+        # vamos identificar qual é a letra mais frequente
+        for language_most_frequent_letter in most_frequent_letters:
+            key = ""
+            for segment in segments:
+                # Determine the most common character in the segment
+                counter = Counter(segment)
+                most_common_char = counter.most_common(1)[0][0]
+
+                two_common_char = counter.most_common(2)
+                # print(two_common_char)
+
+                # agora determinamos qual é a letra da chave referente a coluna que estamos observando
+                # Determine the key character by computing the distance to the most frequent letter
+                key_distance = (ALPHABET.index(most_common_char) -
+                                ALPHABET.index(language_most_frequent_letter)) % 26
+                key += ALPHABET[key_distance]
+            possible_keys.append(key)
+
+    return possible_keys
+
+# def find_key(possible_key_lengths:list[int]=[7], language:str='english', cipher_text:str="") -> list[str]:
+    print("Finding possible keys...")
+
+    ########## Descobrindo a chave ##########
+    language_most_frequent_letters = ['A', 'E']
+
+    if language == 'english':
+        language_most_frequent_letters = ['E', 'T']
+
+    possible_keys: list = []
+
+    for key_length in possible_key_lengths:
+        key = ""
+
+        # Quebrando o texto cifrado em segmentos de tamanho:
+        # seg[len(cipher_text) / key_length][key_length]
+        segments = [cipher_text[i::key_length] for i in range(key_length)]
+
+        # Olhando cada uma das colunas
+        # vamos identificar qual é a letra mais frequente
+        for segment in segments:
+            # Determine the most common characters in the segment
+            counter = Counter(segment)
+            most_common_chars = [char for char,
+                                 count in counter.most_common(2)]
+
+            # Determine the key character by computing the distance to the most frequent letters
+            key_distance_a = (ALPHABET.index(
+                most_common_chars[0]) - ALPHABET.index(language_most_frequent_letters[0])) % 26
+            key_distance_e = (ALPHABET.index(
+                most_common_chars[1]) - ALPHABET.index(language_most_frequent_letters[1])) % 26
+
+            # Determine which distance is closer to the expected value
+            key_distance = key_distance_a
+            if abs(key_distance_e - 5) < abs(key_distance_a - 5):
+                key_distance = key_distance_e
+
+            key += ALPHABET[key_distance]
+        possible_keys.append(key)
+
+    return possible_keys
+
+
+def find_key_length_and_language(max_key_size: int = 20, encrypted_text: str = '') -> tuple[list[int], str]:
+    print("Finding possible key lengths and language...")
+    splitted_texts = []
+    key_size = -1
+    language = "undefined"
+    possible_key_sizes = []
+
+    for i in range(1, max_key_size):
+        splitted_texts = split_text(encrypted_text, i)
+        indexes_of_coincidence = []
+        for text in splitted_texts:
+            indexes_of_coincidence.append(get_IOC(text))
+        # Test indexes to check the length
+        for index in indexes_of_coincidence:
+            if index >= 0.0617 and index <= 0.0697:
+                key_size = i
+                language = "english"
+                print("text is in english. Key Size = ", key_size)
+                break
+            if index >= 0.0725 and index <= 0.082:
+                key_size = i
+                language = "portuguese"
+                print("text is in portuguese. Key Size = ", key_size)
+                break
+        if language != "undefined" and key_size > 0:
+            possible_key_sizes.append(key_size)
+
+    return possible_key_sizes, language
+
+
+def get_IOC(text: List[str]) -> float:
+    """Calculates the index of coincidence of a text.
+
+    Args:
+        text (List[str]): A list containing all the chars of a text.
+
+    Returns:
+        float: The index of coincidence of the text.
+    """
+    N = len(text)
+    # Returns a dict with letter as key and number of occurances as value
+    freqs = Counter(text)
+    freqsum = 0.0
+
+    for letter in ALPHABET:
+        freqsum += freqs[letter] * (freqs[letter] - 1)
+
+    IC = freqsum / (N*(N-1))
+    return IC
 
 
 def split_text(encrypted_text: str, size: int) -> List[List[str]]:
@@ -43,165 +237,13 @@ def split_text(encrypted_text: str, size: int) -> List[List[str]]:
     return splitted_texts
 
 
-def getIOC(text: List[str]) -> float:
-    """Calculates the index of coincidence of a text.
-
-    Args:
-        text (List[str]): A list containing all the chars of a text.
-
-    Returns:
-        float: The index of coincidence of the text.
-    """
-    N = len(text)
-    # Returns a dict with letter as key and number of occurances as value
-    freqs = Counter(text)
-    freqsum = 0.0
-
-    for letter in alph:
-        freqsum += freqs[letter] * (freqs[letter] - 1)
-
-    IC = freqsum / (N*(N-1))
-    return IC
+def read_encrypted_file(file_path: str) -> str:
+    with open(file_path) as f:
+        encrypted_text = f.read()
+    # Text
+    encrypted_text = encrypted_text.replace(" ", "").upper()
+    return encrypted_text
 
 
-####################### Descobrir o tamanho da chave ########################
-splitted_texts = []
-key_size = -1
-language = "undefined"
-possible_key_sizes = []
-
-for i in range(1, max_key_size):
-    splitted_texts = split_text(encrypted_text, i)
-    indexes_of_coincidence = []
-    for text in splitted_texts:
-        indexes_of_coincidence.append(getIOC(text))
-    # Test indexes to check the length
-    for index in indexes_of_coincidence:
-        # if isclose(index, english_index, abs_tol=0.005):
-        if index >= 0.0617 and index <= 0.0697:
-            key_size = i
-            language = "english"
-            print("text is in english. Key Size = ", key_size)
-            break
-        # if isclose(index, portuguese_index, abs_tol=0.005):
-        if index >= 0.0725 and index <= 0.082:
-            key_size = i
-            language = "portuguese"
-            print("text is in portuguese. Key Size = ", key_size)
-            break
-    if language != "undefined" and key_size > 0:
-        possible_key_sizes.append(key_size)
-
-####################### Decifrar o texto #######################
-
-
-# frequency analysis in each bin (we will find that each bin is just like the language frequency only shifted by a number. That number is the letter of the alphabet)
-english_most_common_letters = ['E', 'A', 'I', 'N', 'O', 'S']
-portuguese_most_common_letters = ['A', 'E', 'O', 'S', 'R', 'I']
-
-import string
-
-ALPHABET = string.ascii_uppercase
-
-# Method to generate the Vigenere square
-def generate_vigenere_square():
-    vigenere_square = {}
-    for i in range(len(ALPHABET)):
-        shifted_alphabet = ALPHABET[i:] + ALPHABET[:i]
-        vigenere_square[ALPHABET[i]] = shifted_alphabet
-    return vigenere_square
-
-# Method to decrypt the Vigenere cipher
-def decrypt_vigenere(cipher_text, key_length):
-    vigenere_square = generate_vigenere_square()
-    segments = [cipher_text[i::key_length] for i in range(key_length)]
-    decrypted_segments = []
-
-    for segment in segments:
-        # Determine the most common character in the segment
-        counter = Counter(segment)
-        most_common_char = counter.most_common(1)[0][0]
-
-        # Calculate the shift amount for the segment
-        shift_amount = ALPHABET.index(most_common_char) - ALPHABET.index('E')
-        if shift_amount < 0:
-            shift_amount += len(ALPHABET)
-
-        # Decrypt the segment
-        decrypted_segment = ''
-        for i in range(len(segment)):
-            key_char = ALPHABET[(i + ALPHABET.index(most_common_char)) % len(ALPHABET)]
-            shifted_alphabet = vigenere_square[key_char]
-            shifted_index = (ALPHABET.index(segment[i]) - ALPHABET.index(shifted_alphabet[shift_amount])) % len(ALPHABET)
-            decrypted_segment += ALPHABET[shifted_index]
-
-        decrypted_segments.append(decrypted_segment)
-
-    # Combine the decrypted segments to get the original message
-    decrypted_text = ''
-    for i in range(len(cipher_text)):
-        decrypted_text += decrypted_segments[i % key_length][i // key_length]
-
-    return decrypted_text
-
-print(decrypt_vigenere(encrypted_text, 7))
-
-exit()
-
-for key_size in possible_key_sizes:
-    shifts_per_bin = []
-    print("key size ", key_size)
-    text_splitted_into_bins = split_text(encrypted_text, size=key_size)
-    print("Text splitted bins ", len(text_splitted_into_bins))
-
-    # A list of lists, where each inside list has a possible decripted bin
-    possible_decripted_bins = []
-
-    bin_number = 0
-    for bin in text_splitted_into_bins:
-        bin_number += 1
-        possible_bin_decripted = []
-        # Most frequent char in the bin:
-        most_freq_char = ''
-        biggest_occurance = 0
-        for key, value in Counter(bin).items():
-            if value > biggest_occurance:
-                biggest_occurance = value
-                most_freq_char = key
-        # Difference between the most common and the possble letters it can be:
-        possible_bin_shifts = []
-        if language == "english":
-            for l in english_most_common_letters:
-                shift = abs(ord(most_freq_char) - ord(l)) - 1
-                possible_bin_shifts.append(shift)
-        if language == "portuguese":
-            for l in portuguese_most_common_letters:
-                shift = abs(ord(most_freq_char) - ord(l)) - 1
-                possible_bin_shifts.append(shift)
-        # Decript bin using the possile shifts
-
-        print(f"Bin number {bin_number}: ", possible_bin_shifts)
-
-        # Transform the bin (list) to text (str)
-        # bin_text = ""
-        # for l in bin:
-        #     bin_text += l
-
-        # # Make de shifts, decripting the bin for every possible shift
-        # for shift in possible_bin_shifts:
-        #     transformations = str.maketrans(
-        #         alph, alph[-shift:] + alph[: -shift])
-        #     decripted_bin_text = bin_text.translate(transformations)
-
-        #     # Returning from str to list
-        #     decripted_bin = []
-        #     for l in decripted_bin_text:
-        #         decripted_bin.append(l)
-
-        #     possible_bin_decripted.append(decripted_bin)
-
-        # possible_decripted_bins.append(possible_bin_decripted)
-
-    # Combine the decrypted segments (possible_decripted_bins) to get the decrypted message.
-    # for k in range(key_size):
-    #     for possible_bin_decripted in possible_decripted_bins:
+if __name__ == '__main__':
+    main()
